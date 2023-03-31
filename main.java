8d1da446a501b663dc521ff4f6ca1946fe4e5e73
lidhuna <129526453+lidhuna@users.noreply.github.com>
2023-03-31 20:08:55 +0530

    Day 3 assignment submitted

---
+++ main.java
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    int n=sc.nextInt();
+	    if(n>=0)
+	    {
+		System.out.println("The number is posotive");
+	}
+	else
+	{
+	    	System.out.println("The number is negative");
+	    
+	}
+	}
+}
